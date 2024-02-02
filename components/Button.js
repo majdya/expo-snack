504e382649faa6be593b074b4c8403e2a8bdfe19
majdyaq <majd@feelit.tech>
2024-02-02 12:47:05 +0200

    style button based on theme

--- components/Button.js
+++ components/Button.js
@@ -1,6 +1,22 @@
 import { StyleSheet, View, Pressable, Text } from "react-native";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
-export default function Button({ label }) {
+export default function Button({ label, theme }) {
+    if (theme === "primary") {
+        console.log("theme", theme);
+        return (<View style={[styles.buttonContainer, { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 }]}>
+            <Pressable style={[styles.button, { backgroundColor: "#fff" }]} onPress={() => alert("Pressed")}>
+                <FontAwesome
+                    name="picture-o"
+                    size={18}
+                    color="#25292e"
+                    style={styles.buttonIcon}
+                />
+                <Text style={[styles.buttonLabel, { color: "#25292e" }]}>{label}</Text>
+            </Pressable>
+        </View>
+        );
+    }
     return (
         <View style={styles.buttonContainer}>
             <Pressable style={styles.button} onPress={() => alert("Pressed")}>
@@ -14,18 +30,19 @@ const styles = StyleSheet.create({
     buttonContainer: {
         width: 320,
         hight: 68,
-        marginHorizontal: 20,
+        marginHorizontal: 30,
         alignItems: "center",
         justifyContent: "center",
         padding: 3
     },
     button: {
-        borderRadios: 10,
+        borderRadius: 10,
         width: '100%',
         hight: '100%',
         alignItems: 'center',
         justifyContent: "center",
-        flexDirection: 'row'
+        flexDirection: 'row',
+        paddingVertical: 10,
     },
     buttonIcon: {
         paddingRight: 8,
