d2a385663bbbf6bcafe87b6a2c3daf3009168cab
majdyaq <majd@feelit.tech>
2024-02-02 22:41:41 +0200

    conditionally render pick |image buttons

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ const PlaceholderImage = require("./assets/images/background-image.png");
 export default function App() {
 
   const [selectedImage, setSelectedImage] = useState(null);
+  const [showAppOptions, setShowAppOptions] = useState(false);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -18,8 +19,8 @@ export default function App() {
       quality: 1
     });
     if (!result.canceled) {
-      setSelectedImage
-        (result.assets[0].uri);
+      setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
     } else {
       alert('You did not select any image.');
     }
@@ -30,10 +31,13 @@ export default function App() {
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
-      <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
-        <Button label="Use this photo" />
-      </View>
+      {showAppOptions ? (<View />) : (
+
+        <View style={styles.footerContainer}>
+          <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+          <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
+        </View>
+      )}
       <StatusBar style="auto" />
     </View>
   );
